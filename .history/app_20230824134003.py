38542eb0757820b70902bb5a65246cdf3e5484f4
hasnainzxc <hasnainzxc@icloud.com>
2023-08-24 02:19:20 -0700

    Added langchain convsersation chain memory

--- .history/app_20230824134003.py
+++ .history/app_20230824134003.py
@@ -4,7 +4,6 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
 
 
 
@@ -33,19 +32,7 @@ def get_vectorstore(text_chunks):
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
-
-def get_conversation_chain(vectorstore):
-    # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
+    
 
 def main():
     load_dotenv()
@@ -72,12 +59,6 @@ def main():
                 # Create vector store
                 
                 vectorstore = get_vectorstore(text_chunks)
-                
-                
-             # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
 
 if __name__ == '__main__':
     main()
--- .history/app_20230824134921.py
+++ .history/app_20230824134921.py
@@ -37,15 +37,7 @@ def get_vectorstore(text_chunks):
 def get_conversation_chain(vectorstore):
     # llm = ChatOpenAI()
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
+    
 
 def main():
     load_dotenv()
